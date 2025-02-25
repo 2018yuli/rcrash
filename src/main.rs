0ac7135d41f45c197ae0dca535677e3f399b54bb
l898974766 <l898974766@outlook.com>
2025-02-25 11:58:23 +0000

    20250225

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,41 @@
-#[derive(Debug)]
-enum Alphabet {
-    A,
-    B,
+
+// 函数
+fn fibonatic(n: u64) -> u64 {
+    if n < 2 {
+        n
+    } else {
+        fibonatic(n - 1) + fibonatic(n - 2)
+    }
 }
 
+// 方法
+#[derive(Debug)]
+struct Point {
+    x: u64,
+    y: u64,
+}
+
+impl Point {
+    // 构造方法
+    fn new(x: u64, y: u64) -> Point {
+        Point { x, y }
+    }
+
+    fn get_x(&self) -> u64 {
+        self.x
+    }
+
+    fn set_x(&mut self, x: u64) {
+        self.x = x
+    }
+}
 
 fn main() {
-    let mut letter = Alphabet::A;
+    println!("fibonatic(10) = {}", fibonatic(10));
 
-    // while let 很少被用到
-    while let Alphabet::A = letter {
-        println!("{:?}", letter);
-        letter = Alphabet::B;
-    }
+    let mut p = Point::new(10, 20);
+    println!("{:?}, Point.x = {}, p.get_x() = {}", p, p.x, p.get_x());
+
+    p.set_x(12);
+    println!("p = {:?}", p)
 }
