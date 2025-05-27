ad2d8284fc9f0e8fbd120741d168a6b5db623569
l898974766 <l898974766@outlook.com>
2025-05-27 06:20:29 +0000

    20250527

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,44 @@
-// 0 -> 1 \
-//         | -> 4
-// 2 -> 3 /
 
-use std::rc::Rc;
-
-
-#[derive(Debug)]
-enum List {
-    Cons(i32, Rc<List>),
-    Nil
-}
 
 fn main() {
-    // 带引入计数器的只能指针
+    let mut v: Vec<i32> = Vec::new();
+    for i in  1..11 {
+        v.push(i);
+    }
+    println!("{:?}", v);
 
-    // RC: 实现一个值可以有多个所有者，Rc::new, Rc::clone（底层的计数+1）
-    let four = Rc::new(List::Cons(4, Rc::new(List::Nil)));
-    // let zero_one = List::Cons(0, Rc::new(List::Cons(1, four.clone())));
-    // 一种更为推荐的，直观的做法，防止和 数据的 clone 混淆
-    let zero_one = List::Cons(0, Rc::new(List::Cons(1, Rc::clone(&four))));
-    let two_three = List::Cons(2, Rc::new(List::Cons(3, four)));
-    println!("{:?}", zero_one);
-    println!("{:?}", two_three);
+    // 使用 宏来创建 向量
+    let mut v1 = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    println!("{:?}", v1);
+    println!("v1.length = {}, v1.capacity {}", v1.len(), v1.capacity());
+
+    // pop 操作
+    println!("{:?}", v1.pop());
+    println!("after pop : v1.length = {}, v1.capacity {}", v1.len(), v1.capacity());
+
+    // 自动扩容
+    v1.push(10);
+    v1.push(11);
+    println!("after extend : v1.length = {}, v1.capacity {}", v1.len(), v1.capacity());
+
+    // 迭代操作
+    for i in 0..v1.len() {
+        print!("v1[{:?}] = {:?} \t", i, v1[i]);
+    }
+    println!("");
+
+    // 累乘2
+    for e in v1.iter_mut() {
+        *e *=  2;
+    }
+
+    // 迭代操作2
+    let mut idx = 0;
+    for e in v1.iter() {
+        print!("v1[{:?}] = {:?} \t", idx, *e);
+        idx+=1;
+    }
+    println!("");
+
+    
 }
