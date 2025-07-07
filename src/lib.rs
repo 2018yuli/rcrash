b16b4c2b03862303146501fc4c876729032db005
l898974766 <l898974766@outlook.com>
2025-07-07 02:19:21 +0000

    clear workspace
    Signed-off-by: l898974766 <l898974766@outlook.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,1 @@
-pub mod cli;
-mod processes;
-pub mod utils;
 
-pub use cli::{Base64SubCommand, Opts, SubCommand, TextSubCommand};
-
-pub use processes::{
-    process_csv, process_csv_common, process_decode, process_encode, process_generate_password,
-};
